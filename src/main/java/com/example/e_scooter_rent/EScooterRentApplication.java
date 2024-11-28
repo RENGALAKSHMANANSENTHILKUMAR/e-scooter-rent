6f4ed5ed0fba47099f247a1bc3546da553b1fa46
RENGALAKSHMANAN SENTHILKUMAR <lakshmananrenga0@gmail.com>
2024-11-28 19:01:48 +0530

    init commit

---
+++ src/main/java/com/example/e_scooter_rent/EScooterRentApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_scooter_rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EScooterRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EScooterRentApplication.class, args);
+	}
+
+}
